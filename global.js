dc0bdebea5e0750ead2c20dfa7360911e516c99f
xjzh123 <37945610+xjzh123@users.noreply.github.com>
2023-11-12 16:39:57 +0800

    delete 2 files and update 1 file

--- global.js
+++ global.js
@@ -17,6 +17,63 @@ let allowImages = false
 
 let unread = 0
 
+
+const isAtBottom = () => (
+  (window.innerHeight + window.scrollY) >= (document.body.scrollHeight - 1)
+)
+
+const scrollToBottom = () => {
+  window.scrollTo(0, document.body.scrollHeight)
+}
+
+/**
+ * @param {string} selectors
+ * @returns {Element|HTMLElement|null}
+ */
+const $css = (selectors) => {
+  return document.querySelector(selectors)
+}
+
+/**
+ * @param {string} elementId
+ * @returns {HTMLElement|null}
+ */
+const $id = (elementId) => {
+  return document.getElementById(elementId)
+}
+
+/**
+ * @param {string} key
+ * @returns {any}
+ */
+const localStorageGet = (key) => {
+  const value = window.localStorage[key]
+  if (value) {
+    return JSON.parse(value)
+  }
+}
+
+/**
+ * @param {string} key
+ * @param {any} value
+ */
+const localStorageSet = (key, value) => {
+  window.localStorage[key] = JSON.stringify(value)
+}
+
+
+/** @type {HTMLTextAreaElement} */
+const input = $id("chatinput")
+
+/** @type {HTMLElement} */
+const footer = $id("footer")
+
+/** @type {HTMLDivElement} */
+const messagesEl = $id("messages")
+
+/** @type {HTMLDivElement} */
+const usersEl = $id("users")
+
 /**
  * @typedef Msg
  * @type {import("./engine").Msg}
